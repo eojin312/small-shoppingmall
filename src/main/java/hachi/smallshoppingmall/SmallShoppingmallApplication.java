56575785f6d7a127413642fbd6867b5f958af799
hachi <eojin312@naver.com>
2020-07-09 20:15:17 +0900

    쇼핑몰 프로젝트 첫 커밋

---
+++ src/main/java/hachi/smallshoppingmall/SmallShoppingmallApplication.java
@@ -0,0 +1,13 @@
+package hachi.smallshoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallShoppingmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallShoppingmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hachi/smallshoppingmall/SmallShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package hachi.smallshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallShoppingmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
